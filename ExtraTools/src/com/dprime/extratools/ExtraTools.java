6285fd49c1729f90e4e41bf6034c291a4766f375
Zlatko Posavec <posavec.zlatko01@gmail.com>
2014-06-27 13:38:34 +0200

    commit #1 of extratools proj
    Signed-off-by: Zlatko Posavec <posavec.zlatko01@gmail.com>

---
+++ ExtraTools/src/com/dprime/extratools/ExtraTools.java
@@ -0,0 +1,16 @@
+package com.dprime.extratools;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = ExtraTools.MODID, version = ExtraTools.VERSION)
+public class ExtraTools {
+	public static final String MODID = "extratools";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    }
+}
